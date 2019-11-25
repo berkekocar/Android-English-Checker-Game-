899c1de840d1b016d936e2b723e96cd4db0c6e6b
berkekocar <e.berkekocar@gmail.com>
2019-11-25 14:11:45 +0000

    Bracket 1

---
+++ app/src/main/java/com/example/android_development_assignment_2/CustomView.java
@@ -0,0 +1,77 @@
+package com.example.android_development_assignment_2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class CustomView extends View {
+    private Paint black, white;
+    private Rect square;
+    private boolean touches[];
+    private float touchx[];
+    private float touchy[];
+    private int first;
+    private boolean touch;
+
+    public CustomView(Context c) {
+        super(c);
+        init();
+    }
+
+    public CustomView(Context c, AttributeSet as) {
+        super(c, as);
+        init();
+    }
+
+    public CustomView(Context c, AttributeSet as, int default_style) {
+        super(c, as, default_style);
+        init();
+    }
+
+    private void init() {
+
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int size = 0;
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        if (width > height) {
+            size = height;
+        } else {
+            size = width;
+        }
+        setMeasuredDimension(size, size);
+
+        int widthWithoutPadding = width - getPaddingLeft() - getPaddingRight();
+        int heigthWithoutPadding = height - getPaddingTop() - getPaddingBottom();
+
+        if (widthWithoutPadding > heigthWithoutPadding) {
+            size = heigthWithoutPadding;
+        } else {
+            size = widthWithoutPadding;
+        }
+
+        setMeasuredDimension(size + getPaddingLeft() + getPaddingRight(), size + getPaddingTop() + getPaddingBottom());
+    }
+}
